b5e5ccebbd82ed003c09caf968d2d9a1c60f7989
Kevin Carwile <kevin@miller-media.com>
2020-08-24 15:06:39 -0700

    add new do_copy() method to TodoList controller

--- classes/Controllers/TodoController.php
+++ classes/Controllers/TodoController.php
@@ -30,4 +30,28 @@ class _TodoController extends ActiveRecordController
 	{
 		
 	}
+
+	/**
+	 * Copy a record
+	 *
+	 */
+	public function do_copy()
+	{
+		$controller = $this;
+		$class = $this->recordClass;
+		
+		if ( ! $record ) {
+			try	{
+				$record = $class::load( isset( $_REQUEST['id'] ) ? $_REQUEST['id'] : 0 );
+			}
+			catch( \OutOfRangeException $e ) { 
+ 				echo $this->error( __( 'The record could not be loaded.', 'mwp-framework' ) . ' Class: ' . $this->recordClass . ' ' . ', ID: ' . ( (int) $_REQUEST['id'] ) );
+				return;
+			}
+		}
+
+		$copy = $record->copy();
+		wp_redirect( $controller->getUrl([ 'do' => 'edit', 'id' => $copy->id() ]) );
+		exit;
+	}	
 }
